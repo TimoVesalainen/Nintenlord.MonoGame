155125d2bdb47d522ad93cb62e680fb0c4261195
Timo Vesalainen <timovesalainen@hotmail.com>
2021-03-15 19:45:25 +0200

    Added few helpers for handling texture mipmaps

--- Nintenlord.MonoGame/Graphics/Textures/TextureExtensions.cs
+++ Nintenlord.MonoGame/Graphics/Textures/TextureExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 
 namespace Nintenlord.MonoGame.Graphics.Textures
@@ -36,6 +37,101 @@ namespace Nintenlord.MonoGame.Graphics.Textures
         public static void SetPixel<T>(this Texture2D texture, T pixel, int x, int y) where T : struct
         {
             texture.SetData(0, new Rectangle(x, y, 1, 1), new[] { pixel }, 0, 1);
+        }
+
+        public static void SaveMipmapLevels(this Texture2D texture, GraphicsDevice device, Func<int, string> filenameGenerator = null)
+        {
+            filenameGenerator = filenameGenerator ?? (i => $"level_{i}.png");
+
+            var width = texture.Width;
+            var height = texture.Height;
+
+            var array = new Color[width * height];
+            for (int i = 0; i < texture.LevelCount; i++)
+            {
+                var smallerTexture = new Texture2D(device, width, height);
+
+                texture.GetData(i, null, array, 0, width * height);
+
+                smallerTexture.SetData(array, 0, width * height);
+
+                smallerTexture.SaveAsPng(System.IO.File.OpenWrite(filenameGenerator(i)), width, height);
+                width /= 2;
+                height /= 2;
+            }
+        }
+
+        /// <summary>
+        /// Prevents color bleeding, unlike default mipmap generation
+        /// </summary>
+        public static void CreateMipmaps(this Texture2D texture, int maxLevel)
+        {
+            var width = texture.Width;
+            var height = texture.Height;
+
+            var originalArray = new Color[width * height];
+            texture.GetData(originalArray);
+
+            Rectangle rect = Rectangle.Empty;
+
+            for (int i = 1; i < maxLevel; i++)
+            {
+                if (width > 1)
+                    width /= 2;
+                if (height > 1)
+                    height /= 2;
+
+                var array2 = new Color[width * height];
+
+                rect.Width = texture.Width / width;
+                rect.Height = texture.Height / height;
+
+                for (int y = 0; y < height; y++)
+                {
+                    var rowStart = width * y;
+                    for (int x = 0; x < width; x++)
+                    {
+                        int index = rowStart + x;
+
+                        rect.X = x * rect.Width;
+                        rect.Y = y * rect.Height;
+
+                        int red = 0;
+                        int green = 0;
+                        int blue = 0;
+                        int alpha = 0;
+                        foreach (var pixel in IterateRect(originalArray, texture.Width, rect))
+                        {
+                            red += pixel.R;
+                            green += pixel.G;
+                            blue += pixel.B;
+                            alpha += pixel.A;
+                        }
+                        int pixelCount = rect.Width * rect.Height;
+                        array2[index] = new Color(
+                            red / pixelCount,
+                            green / pixelCount,
+                            blue / pixelCount,
+                            alpha / pixelCount);
+                    }
+                }
+
+                texture.SetData(i, null, array2, 0, array2.Length);
+            }
+        }
+
+        public static IEnumerable<T> IterateRect<T>(T[] pixels, int width, Rectangle rect)
+        {
+            for (int y = rect.Top; y < rect.Bottom; y++)
+            {
+                int rowStart = width * y;
+                for (int x = rect.Left; x < rect.Right; x++)
+                {
+                    int index = rowStart + x;
+
+                    yield return pixels[index];
+                }
+            }
         }
     }
 }
