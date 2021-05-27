bcae4a67468c14d9415ae4aae0ca24a895f9c37f
Timo Vesalainen <timovesalainen@hotmail.com>
2021-05-27 22:30:21 +0300

    Added variant of 3D noise

---
+++ Nintenlord.MonoGame/Noise/AccumulateNoise3D.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nintenlord.MonoGame.Noise
+{
+    public sealed class AccumulateNoise3D : AccumulateNoiseBase
+    {
+        private const int offsetX = 499;
+        private const int offsetY = 506;
+        private const int offsetZ = 515;
+
+        private readonly Vector3 offset = new Vector3(offsetX, offsetY, offsetZ);
+        private readonly INoise3D noise;
+        private readonly Matrix rotationMatrix;
+
+
+        public AccumulateNoise3D(INoise3D noise, Quaternion rotation, float detail, float roughness)
+            : base(detail, roughness)
+        {
+            this.noise = noise;
+
+            rotationMatrix = Matrix.CreateFromQuaternion(rotation);
+        }
+
+        public float SumOctaves(Vector3 position)
+        {
+            float total = 0.0f;
+
+            for (int i = 0; i < octaveLevels.Length; i++)
+            {
+                var level = octaveLevels[i];
+
+                // rotate the coordinates.
+                // reduces correlation between octaves.
+                position = Vector3.Transform(position, rotationMatrix);
+
+                var noiseLevel = (float)noise.Noise(position * level.Frequency);
+
+                noiseLevel *= level.Amplitude;
+
+                // if this is the last 'partial' octave,
+                // reduce its contribution accordingly.
+                if (level.PartialOctaveFactor < 1)
+                {
+                    noiseLevel *= level.PartialOctaveFactor;
+                }
+
+                total += noiseLevel;
+
+                // offset the coordinates by prime numbers, with prime difference.
+                // reduces correlation between octaves.
+                position += offset;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Nintenlord.MonoGame/Noise/PerlinNoise3D.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Nintenlord.MonoGame.Geometry;
+using System;
+
+namespace Nintenlord.MonoGame.Noise
+{
+    public sealed class PerlinNoise3D : AbstractPerlinNoise3D
+    {
+        readonly Vector3[,,] gradients;
+        readonly int mask;
+
+        public PerlinNoise3D(int hexLength, Random random)
+        {
+            int length = 1 << hexLength;
+            mask = length - 1;
+            gradients = new Vector3[length, length, length];
+            for (int z = 0; z < length; z++)
+            {
+                for (int y = 0; y < length; y++)
+                {
+                    for (int x = 0; x < length; x++)
+                    {
+                        gradients[z, y, x] = random.GetRandomUnitVector();
+                    }
+                }
+            }
+        }
+
+
+        protected override float Fade(float value)
+        {
+            return value * value * value * (value * (value * 6 - 15) + 10);
+        }
+
+        protected override Vector3 GradientVector(int x, int y, int z)
+        {
+            return gradients[z & mask, y & mask, x & mask];
+        }
+    }
+}
