22488f3ecc76e4a6a3b299cc0215e1f5a1f2153a
Timo Vesalainen <timovesalainen@hotmail.com>
2021-05-29 09:33:48 +0300

    Used fields for source of gradients

--- Nintenlord.MonoGame/Noise/PerlinNoise2D.cs
+++ Nintenlord.MonoGame/Noise/PerlinNoise2D.cs
@@ -1,61 +1,61 @@
 ﻿using Microsoft.Xna.Framework;
+using Nintenlord.MonoGame.Geometry.Fields;
 using System;
 
 namespace Nintenlord.MonoGame.Noise
 {
-    public sealed class PerlinNoise2D : AbstractPerlinNoise2D
+    public sealed class PerlinNoise2D : INoise2D
     {
-        readonly byte seed;
-        private static readonly int[] permuteLookup = new int[512];
-        private static readonly int[] permutationTable =
-            new[]
-                {
-                    151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7,
-                    225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6,
-                    148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35,
-                    11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171,
-                    168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231,
-                    83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245,
-                    40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76,
-                    132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86,
-                    164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
-                    5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47,
-                    16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2,
-                    44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39,
-                    253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218,
-                    246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162,
-                    241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181,
-                    199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150,
-                    254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128,
-                    195, 78, 66, 215, 61, 156, 180
-                };
+        readonly IVectorField2iTo2v gradients;
 
-        public PerlinNoise2D(byte seed)
+        public PerlinNoise2D(IVectorField2iTo2v gradients)
         {
-            this.seed = seed;
-
-            Array.Copy(permutationTable, 0, permuteLookup, 0, permutationTable.Length);
-            Array.Copy(permutationTable, 0, permuteLookup, permutationTable.Length, permutationTable.Length);
+            this.gradients = gradients;
         }
 
-        protected override float Fade(float value)
+        private float Fade(float value)
         {
             return value * value * value * (value * (value * 6 - 15) + 10);
         }
 
-        protected override Vector2 GradientVector(int x, int y)
+        public double Noise(Vector2 position)
         {
-            x &= 0xFF;
-            y &= 0xFF;
-            int hash = permuteLookup[permuteLookup[permuteLookup[x + seed] + y]] & 15;
+            var flooredX = Math.Floor(position.X);
+            var flooredY = Math.Floor(position.Y);
 
-            float resultX = 1;
-            float resultY = 1;
+            int xMin = (int)flooredX;
+            int yMin = (int)flooredY;
 
-            float u = hash < 8 ? resultX : resultY;
-            float v = hash < 4 ? resultY : hash == 12 || hash == 14 ? resultX : 0;
+            Vector2 inCellPosition = position - new Vector2(xMin, yMin);
 
-            return new Vector2((hash & 1) == 0 ? u : -u, (hash & 2) == 0 ? v : -v);
+            var gradLeftBottom = gradients[xMin, yMin];
+            var gradLeftTop = gradients[xMin, yMin + 1];
+            var gradRightBottom = gradients[xMin + 1, yMin];
+            var gradRightTop = gradients[xMin + 1, yMin + 1];
+
+            Vector2 cornerOffsetPositionLeftBottom = inCellPosition;
+            Vector2 cornerOffsetPositionLeftTop = new Vector2(
+                inCellPosition.X,
+                1 - inCellPosition.Y);
+            Vector2 cornerOffsetPositionRightBottom = new Vector2(
+                1 - inCellPosition.X,
+                inCellPosition.Y);
+            Vector2 cornerOffsetPositionRightTop = new Vector2(
+                1 - inCellPosition.X,
+                1 - inCellPosition.Y);
+
+            var u = Fade(inCellPosition.X);
+            var v = Fade(inCellPosition.Y);
+
+            var leftBottomDot = Vector2.Dot(cornerOffsetPositionLeftBottom, gradLeftBottom);
+            var leftTopDot = Vector2.Dot(cornerOffsetPositionLeftTop, gradLeftTop);
+            var rightBottomDot = Vector2.Dot(cornerOffsetPositionRightBottom, gradRightBottom);
+            var rightTopDot = Vector2.Dot(cornerOffsetPositionRightTop, gradRightTop);
+
+            var top = MathHelper.Lerp(leftTopDot, rightTopDot, u);
+            var bottom = MathHelper.Lerp(leftBottomDot, rightBottomDot, u);
+
+            return MathHelper.Lerp(bottom, top, v);
         }
     }
 }
--- Nintenlord.MonoGame/Noise/PerlinNoise3D.cs
+++ Nintenlord.MonoGame/Noise/PerlinNoise3D.cs
@@ -1,40 +1,99 @@
 ﻿using Microsoft.Xna.Framework;
-using Nintenlord.MonoGame.Geometry;
+using Nintenlord.MonoGame.Geometry.Fields;
 using System;
 
 namespace Nintenlord.MonoGame.Noise
 {
-    public sealed class PerlinNoise3D : AbstractPerlinNoise3D
+    public sealed class PerlinNoise3D : INoise3D
     {
-        readonly Vector3[,,] gradients;
-        readonly int mask;
+        readonly IVectorField3iTo3v gradients;
 
-        public PerlinNoise3D(int hexLength, Random random)
+        public PerlinNoise3D(IVectorField3iTo3v gradients)
         {
-            int length = 1 << hexLength;
-            mask = length - 1;
-            gradients = new Vector3[length, length, length];
-            for (int z = 0; z < length; z++)
-            {
-                for (int y = 0; y < length; y++)
-                {
-                    for (int x = 0; x < length; x++)
-                    {
-                        gradients[z, y, x] = random.GetRandomUnitVector();
-                    }
-                }
-            }
+            this.gradients = gradients;
         }
 
-
-        protected override float Fade(float value)
+        private float Fade(float value)
         {
             return value * value * value * (value * (value * 6 - 15) + 10);
         }
 
-        protected override Vector3 GradientVector(int x, int y, int z)
+
+        public double Noise(Vector3 position)
         {
-            return gradients[z & mask, y & mask, x & mask];
+            var flooredX = Math.Floor(position.X);
+            var flooredY = Math.Floor(position.Y);
+            var flooredZ = Math.Floor(position.Z);
+
+            int xMin = (int)flooredX;
+            int yMin = (int)flooredY;
+            int zMin = (int)flooredZ;
+
+            var inCellPosition = position - new Vector3(xMin, yMin, zMin);
+
+            var gradLeftBottomNear = gradients[xMin, yMin, zMin];
+            var gradLeftTopNear = gradients[xMin, yMin + 1, zMin];
+            var gradRightBottomNear = gradients[xMin + 1, yMin, zMin];
+            var gradRightTopNear = gradients[xMin + 1, yMin + 1, zMin];
+            var gradLeftBottomFar = gradients[xMin, yMin, zMin + 1];
+            var gradLeftTopFar = gradients[xMin, yMin + 1, zMin + 1];
+            var gradRightBottomFar = gradients[xMin + 1, yMin, zMin + 1];
+            var gradRightTopFar = gradients[xMin + 1, yMin + 1, zMin + 1];
+
+            Vector3 cornerOffsetPositionLeftBottomNear = inCellPosition;
+            Vector3 cornerOffsetPositionLeftTopNear = new Vector3(
+                inCellPosition.X,
+                1 - inCellPosition.Y,
+                inCellPosition.Z);
+            Vector3 cornerOffsetPositionRightBottomNear = new Vector3(
+                1 - inCellPosition.X,
+                inCellPosition.Y,
+                inCellPosition.Z);
+            Vector3 cornerOffsetPositionRightTopNear = new Vector3(
+                1 - inCellPosition.X,
+                1 - inCellPosition.Y,
+                inCellPosition.Z);
+
+            Vector3 cornerOffsetPositionLeftBottomFar = new Vector3(
+                inCellPosition.X,
+                inCellPosition.Y,
+                1 - inCellPosition.Z);
+            Vector3 cornerOffsetPositionLeftTopFar = new Vector3(
+                inCellPosition.X,
+                1 - inCellPosition.Y,
+                inCellPosition.Z);
+            Vector3 cornerOffsetPositionRightBottomFar = new Vector3(
+                1 - inCellPosition.X,
+                inCellPosition.Y,
+                1 - inCellPosition.Z);
+            Vector3 cornerOffsetPositionRightTopFar = new Vector3(
+                1 - inCellPosition.X,
+                1 - inCellPosition.Y,
+                1 - inCellPosition.Z);
+
+            var u = Fade(inCellPosition.X);
+            var v = Fade(inCellPosition.Y);
+            var w = Fade(inCellPosition.Z);
+
+            var leftBottomNearDot = Vector3.Dot(cornerOffsetPositionLeftBottomNear, gradLeftBottomNear);
+            var leftTopNearDot = Vector3.Dot(cornerOffsetPositionLeftTopNear, gradLeftTopNear);
+            var rightBottomNearDot = Vector3.Dot(cornerOffsetPositionRightBottomNear, gradRightBottomNear);
+            var rightTopNearDot = Vector3.Dot(cornerOffsetPositionRightTopNear, gradRightTopNear);
+
+            var leftBottomFarDot = Vector3.Dot(cornerOffsetPositionLeftBottomFar, gradLeftBottomFar);
+            var leftTopFarDot = Vector3.Dot(cornerOffsetPositionLeftTopFar, gradLeftTopFar);
+            var rightBottomFarDot = Vector3.Dot(cornerOffsetPositionRightBottomFar, gradRightBottomFar);
+            var rightTopFarDot = Vector3.Dot(cornerOffsetPositionRightTopFar, gradRightTopFar);
+
+            var topNear = MathHelper.Lerp(leftTopNearDot, rightTopNearDot, u);
+            var bottomNear = MathHelper.Lerp(leftBottomNearDot, rightBottomNearDot, u);
+            var topFar = MathHelper.Lerp(leftTopFarDot, rightTopFarDot, u);
+            var bottomFar = MathHelper.Lerp(leftBottomFarDot, rightBottomFarDot, u);
+
+            var near = MathHelper.Lerp(bottomNear, topNear, v);
+            var far = MathHelper.Lerp(bottomFar, topFar, v);
+
+            return MathHelper.Lerp(near, far, w);
         }
     }
 }
