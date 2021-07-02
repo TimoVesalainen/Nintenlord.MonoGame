38abaf89f6f01a61965e92524b2477cfc30b07ab
Timo Vesalainen <timovesalainen@hotmail.com>
2021-07-02 12:00:48 +0300

    Wrapped monogame matrix to interface matrix

---
+++ Nintenlord.MonoGame/Utility/MatrixWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Nintenlord.Matricis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintenlord.MonoGame.Utility
+{
+    public sealed class MatrixWrapper : IMatrix<float>
+    {
+        readonly Matrix matrix;
+
+        public float this[int x, int y] => matrix[y, x];
+
+        public int Width => 4;
+
+        public int Height => 4;
+    }
+}
