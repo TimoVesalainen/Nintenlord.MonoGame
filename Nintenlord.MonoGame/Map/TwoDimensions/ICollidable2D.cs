2214d640105d8f5dbb87ae2d579820e0ad515ad6
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-24 21:02:40 +0200

    Added interface for collidabtl 2d object

---
+++ Nintenlord.MonoGame/Map/TwoDimensions/ICollidable2D.cs
@@ -0,0 +1,16 @@
+﻿using Nintenlord.MonoGame.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.MonoGame.Map.TwoDimensions
+{
+    public interface ICollidable2D : IPositionable2D
+    {
+        RectangleF BoundingRectangle { get; }
+
+        event EventHandler<Moved2DEventArgs> Moved;
+    }
+}
---
+++ Nintenlord.MonoGame/Map/TwoDimensions/Moved2DEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Nintenlord.MonoGame.Map.TwoDimensions
+{
+    public sealed class Moved2DEventArgs : EventArgs
+    {
+        public Vector2 OldPosition { get; }
+        public Vector2 NewPosition { get; }
+
+        public Moved2DEventArgs(Vector2 oldPosition, Vector2 newPosition)
+        {
+            OldPosition = oldPosition;
+            NewPosition = newPosition;
+        }
+    }
+}
