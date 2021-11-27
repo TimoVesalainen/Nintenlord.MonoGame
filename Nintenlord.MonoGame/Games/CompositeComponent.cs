b3a408c1f9def3607000e1f763e87387d70d14dd
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-27 12:14:16 +0200

    Added component to manage several other components

---
+++ Nintenlord.MonoGame/Games/CompositeComponent.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.MonoGame.Games
+{
+    public sealed class CompositeComponent<TGame> : GameComponent<TGame> where TGame : Game
+    {
+        private readonly Func<IEnumerable<IGameComponent>> getComponents;
+        private readonly List<IGameComponent> components = new();
+
+        public CompositeComponent(TGame game, Func<IEnumerable<IGameComponent>> getComponents) : base(game)
+        {
+            this.getComponents = getComponents ?? throw new ArgumentNullException(nameof(getComponents));
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            foreach (var component in getComponents())
+            {
+                components.Add(component);
+                this.MyGame.Components.Add(component);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                foreach (var component in components)
+                {
+                    this.MyGame.Components.Remove(component);
+                    if (component is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
