7827c5166b4bbcab5fdce065ad60d4cfa411de13
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-27 14:35:11 +0200

    Added helper to map keyboard keys

---
+++ Nintenlord.MonoGame/Input/Keyboard/KeyboardKeyMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Nintenlord.MonoGame.Utility;
+using System.Collections.Generic;
+using GameKeyboard = Microsoft.Xna.Framework.Input.Keyboard;
+
+namespace Nintenlord.MonoGame.Input.Keyboard
+{
+    public sealed class KeyboardKeyMapping<TControl> : IControl<TControl>
+    {
+        readonly Dictionary<Keys, TControl> controls = new();
+
+        public void SetKeymap(Keys key, TControl control)
+        {
+            controls[key] = control;
+        }
+
+        public IEnumerable<TControl> GetCurrent()
+        {
+            KeyboardState keyState = GameKeyboard.GetState();
+            foreach (var (key, control) in controls)
+            {
+                if (keyState.IsKeyDown(key))
+                {
+                    yield return control;
+                }
+            }
+        }
+    }
+}
---
+++ Nintenlord.MonoGame/Utility/IControl.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Nintenlord.MonoGame.Utility
+{
+    public interface IControl<TControl>
+    {
+        IEnumerable<TControl> GetCurrent();
+    }
+}
