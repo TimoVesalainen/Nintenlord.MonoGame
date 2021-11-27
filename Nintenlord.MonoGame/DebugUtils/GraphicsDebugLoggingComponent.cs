d713f05f4421e6c324dd1b2cdfbfaefa11a35090
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-27 12:57:51 +0200

    Added basic component for getting graphics debug messages to log

---
+++ Nintenlord.MonoGame/DebugUtils/GraphicsDebugLoggingComponent.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nintenlord.MonoGame.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.MonoGame.Debug
+{
+    public sealed class GraphicsDebugLoggingComponent<TGame> : GameComponent<TGame> where TGame : Game
+    {
+        ILogger logger;
+        GraphicsDebug debug;
+
+        public GraphicsDebugLoggingComponent(TGame game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            logger = this.MyGame.Services.GetService<ILogger>();
+            debug = this.MyGame.GraphicsDevice.GraphicsDebug;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            while (debug.TryDequeueMessage(out var message))
+            {
+                logger.Log(GetLogLevel(message), ToString(message));
+            }
+        }
+
+        private string ToString(GraphicsDebugMessage message)
+        {
+            return $"{message.Category}/{message.Severity}: {message.Id} {message.IdName}: {message.Message}";
+        }
+
+        private LogLevel GetLogLevel(GraphicsDebugMessage message)
+        {
+            //TODO: Use message.Severity to get this.
+            return LogLevel.Information;
+        }
+    }
+}
