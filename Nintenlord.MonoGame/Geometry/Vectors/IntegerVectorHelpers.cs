08cae0bba8b406105ead349ac7a9b71c63e63f4a
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-25 18:38:49 +0300

    Updated namespaces

--- Nintenlord.MonoGame/Geometry/Vectors/IntegerVectorHelpers.cs
+++ Nintenlord.MonoGame/Geometry/Vectors/IntegerVectorHelpers.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Nintenlord.Collections;
-using Nintenlord.Utility.Primitives;
+using Nintenlord.Numerics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
